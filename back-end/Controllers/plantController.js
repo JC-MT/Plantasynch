19964dc2463606dcce504c3ba4d7272357b3928b
JC-MT <jancarlosmatias@gmail.com>
2022-08-25 11:22:40 -0400

    Add Controller for Create/Update/Delete routes

--- back-end/Controllers/plantController.js
+++ back-end/Controllers/plantController.js
@@ -1,32 +1,85 @@
-const express = require('express')
-const plants = express.Router()
+const express = require('express');
+const plants = express.Router();
 
 const {
   getAllPlants,
-  getPlant
-} = require('../Queries/plants')
+  getPlant,
+  createPlant,
+  updatePlant,
+  deletePlant
+} = require('../Queries/plants');
 
 //INDEX
 plants.get('/', async (req, res) => {
-  const allPlants = await getAllPlants()
+  const allPlants = await getAllPlants();
 
   if (allPlants) {
-    res.status(200).json({ payload: allPlants })
+    res.status(200).json({ payload: allPlants });
   } else {
-    res.status(404).json({ status: 404, error: 'error' })
+    res.status(404).json({ status: 404, error: 'Plants could not be found' });
   }
-})
+});
 
 // SHOW
 plants.get('/:id', async (req, res) => {
-  const { id } = req.params
-  const plant = await getPlant(id)
+  const { id } = req.params;
+  const plant = await getPlant(id);
 
   if (plant) {
-    res.status(200).json({ success: true, payload: plant })
+    res.status(200).json({ success: true, payload: plant });
   } else {
-    res.status(404).json({ success: false, payload: `Plant with id: ${id} could not be found` })
+    res
+      .status(404)
+      .json({
+        success: false,
+        payload: `Plant with id: ${id} could not be found`
+      });
   }
-})
+});
 
-module.exports = plants
+//CREATE
+plants.post('/', async (req, res) => {
+  const newPlant = await createPlant(req.body);
+  if (newPlant) {
+    res.status(200).json({ success: true, payload: newPlant });
+  } else {
+    res
+      .status(404)
+      .send({ success: false, payload: 'Plant could not be created' });
+  }
+});
+
+//UPDATE
+plants.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const updatedPlant = await updatePlant(id, req.body);
+
+  if (updatedPlant.id) {
+    res.status(200).json({ success: true, payload: updatedPlant });
+  } else {
+    res
+      .status(404)
+      .send({
+        success: false,
+        payload: `Plant with id: ${id} could not be updated`
+      });
+  }
+});
+
+//DELETE
+plants.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const deletedPlant = await deletePlant(id);
+  if (deletedPlant.id) {
+    res.status(200).json({ success: true, payload: deletedPlant });
+  } else {
+    res
+      .status(404)
+      .send({
+        success: false,
+        payload: `Plant with id: ${id} could not be deleted`
+      });
+  }
+});
+
+module.exports = plants;
