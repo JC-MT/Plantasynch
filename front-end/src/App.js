d53993d321583f1035f5db5198e2d0fe4e141c43
JC-MT <jancarlosmatias@gmail.com>
2022-08-26 11:34:13 -0400

    Add Navbar boilerplate

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom"; 
 
+// COMPONENTS
+import Navbar from "./Components/Navbar";
+
 // PAGES
 import Welcome from "./Pages/Welcome";
 
 export default function App() {
   return (
     <div className="App">
+      <Navbar/>
       <main>
         <Routes>
           <Route path="/" element={<Welcome />} />
---
+++ front-end/src/Components/Navbar.js
@@ -0,0 +1,48 @@
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <div>
+        <Link to={'/'}>Plantasynch</Link>
+      <Link to={'/'}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6"
+        >
+          <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+          <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+        </svg>
+      </Link>
+      <Link to={'/'}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            fillRule="evenodd"
+            d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </Link>
+      <Link to={'/'}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            fillRule="evenodd"
+            d="M12 4.25a.75.75 0 01.75.75v6.25H19a.75.75 0 010 1.5h-6.25V19a.75.75 0 01-1.5 0v-6.25H5a.75.75 0 010-1.5h6.25V5a.75.75 0 01.75-.75z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </Link>
+    </div>
+  );
+}
