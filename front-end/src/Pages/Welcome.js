fafa3c3a5743e431d20f081c4ada85d254a12fd3
JC-MT <jancarlosmatias@gmail.com>
2022-08-26 11:24:18 -0400

    Add Welcome boilerplate

---
+++ front-end/src/Pages/Welcome.js
@@ -0,0 +1,12 @@
+export default function Welcome(){
+    return(
+        <div>
+            <h1 className="text-[50px] text-center mt-[50px]">
+                Welcome to Plantasynch
+            </h1>
+            <h3>Some future bio</h3>
+            <button>Sign in with email</button>
+            <p>Skip to demo site</p>
+        </div>
+    )
+}
