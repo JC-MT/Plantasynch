e16a5ae20ec8a8e49ee69e7e50d801ad0dc50b74
JC-MT <jancarlosmatias@gmail.com>
2022-09-02 10:28:31 -0400

    Add Tailwind to Welcome page

--- front-end/src/Components/Navbar.js
+++ front-end/src/Components/Navbar.js
@@ -7,6 +7,7 @@ export default function Navbar() {
     home: false,
     explore: false,
     add: false,
+    welcome: false
   })
 
   return (
@@ -56,6 +57,13 @@ export default function Navbar() {
           />
         </svg>
         <span className='text-center pl-[5px]'>Add</span>
+      </Link>
+      <Link to={'/'} className={`${location.pathname === '/' || location.pathname === '/sign-in' ? 'text-black' : 'text-gray-300'} ${effect.welcome && "animate-pulse"}`} onClick={() => {setEffect({...effect, welcome: true });
+      }} onAnimationEnd={() => setEffect({...effect, welcome: false})}>
+      <svg className="navbar-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+        <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm11.378-3.917c-.89-.777-2.366-.777-3.255 0a.75.75 0 01-.988-1.129c1.454-1.272 3.776-1.272 5.23 0 1.513 1.324 1.513 3.518 0 4.842a3.75 3.75 0 01-.837.552c-.676.328-1.028.774-1.028 1.152v.75a.75.75 0 01-1.5 0v-.75c0-1.279 1.06-2.107 1.875-2.502.182-.088.351-.199.503-.331.83-.727.83-1.857 0-2.584zM12 18a.75.75 0 100-1.5.75.75 0 000 1.5z" clipRule="evenodd" />
+      </svg>
+      <span className='text-center'>About</span>
       </Link>
     </div>
   );
--- front-end/src/Pages/Welcome.js
+++ front-end/src/Pages/Welcome.js
@@ -2,13 +2,32 @@ import { Link } from 'react-router-dom';
 
 export default function Welcome(){
     return(
-        <div>
-            <h1 className="text-[50px] text-center mt-[50px]">
+        <div className='flex flex-col'>
+            <img className='place-self-center w-100px h-[100px] p-1 mt-6' src='https://cdn-icons-png.flaticon.com/512/628/628324.png'/>
+            <h1 className="text-[40px] text-center p-2">
                 Welcome to Plantasynch
             </h1>
-            <h3>Some future bio</h3>
-            <Link to='/sign-in'>Sign in with email</Link>
-            <Link to='/my-plants'>Skip to demo site</Link>
+            <p className='indent-8 p-2 text-center'><em className='underline'>Plantasynch</em> is a full-stack web-app that provides users general plant information and suggested plant care for all types of plants.</p>
+            <div className='flex flex-col gap-1 place-items-center drop-shadow-sm'>
+                <Link className='button-style hover:pointer-events' to='/sign-in'>Sign in with email</Link>
+                <Link to='/my-plants'>Skip to demo site</Link>
+            </div>
+            <div className='flex flex-row gap-2 justify-center drop-shadow-sm p-5'>
+                <a className='flex flex-col place-self-center'href="https://github.com/JC-MT/Plantasynch" target="_blank">
+                    <img
+                    className="w-16 h-16 place-self-center"
+                    alt="GitHub"
+                    src="https://pngimg.com/uploads/github/github_PNG58.png"
+                    />
+                </a>
+                <a className='flex flex-col place-self-center' href="https://www.linkedin.com/in/jan-matias/" target="_blank">
+                    <img
+                    className="w-12 h-12 place-self-center"
+                    alt="LinkedIn"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png"
+                    />
+                </a>
+            </div>
         </div>
     )
 }
