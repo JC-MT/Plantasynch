d092c59985b85c2a067d5e6bf1daca69839b401c
Jan Matias <jancarlosmatias@gmail.com>
2023-08-12 11:00:37 -0400

    Improves Footer design and adds mobile design

--- front-end/src/Components/ExploreDetails.js
+++ front-end/src/Components/ExploreDetails.js
@@ -4,21 +4,20 @@ import { useNavigate, useParams, Link } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
-import Footer from '../Pages/Footer';
 const API = process.env.REACT_APP_API_URL;
 
-export default function ExploreDetails({loggedInUser}) {
+export default function ExploreDetails({ loggedInUser }) {
   const [explore, setExplore] = useState({});
   const [newplant, setNewPlant] = useState({
-    name: "",
-    image: "15-20-2-28-3-14-30m.jpeg",
-    origin: "",
-    category: "",
-    ideal_light: "",
-    ideal_watering: "",
+    name: '',
+    image: '15-20-2-28-3-14-30m.jpeg',
+    origin: '',
+    category: '',
+    ideal_light: '',
+    ideal_watering: '',
     last_water: '',
     is_healthy: false,
-    email: "",
+    email: '',
     user_id: 0,
     demo_plant: true,
     actions: [],
@@ -34,11 +33,12 @@ export default function ExploreDetails({loggedInUser}) {
       .get(`${API}/plants/explore/${id}`)
       .then((res) => {
         setExplore(res.data.payload);
-        return res.data.payload
-      }).then((res) => {
+        return res.data.payload;
+      })
+      .then((res) => {
         let demoUser = true;
-        if(loggedInUser.id){
-          demoUser = false
+        if (loggedInUser.id) {
+          demoUser = false;
         }
         setNewPlant({
           ...newplant,
@@ -47,99 +47,137 @@ export default function ExploreDetails({loggedInUser}) {
           category: res.category,
           ideal_light: res.ideallight,
           ideal_watering: res.watering,
-          email: loggedInUser.email || "",
+          email: loggedInUser.email || '',
           user_id: loggedInUser.id || 0,
-          demo_plant: demoUser,
-        })
+          demo_plant: demoUser
+        });
       })
       .catch((err) => {
-        console.log(err)
+        console.log(err);
         navigate('/not-found');
       });
   }, [id, navigate]);
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    document.body.scrollTop = document.documentElement.scrollTop = 0
+    event.preventDefault();
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
     axios
       .post(`${API}/plants`, newplant)
       .then((res) => {
-        notify(true)
-        setNewPlant(res.data)
-        setTimeout(() => navigate('/my-plants'), 4000)
+        notify(true);
+        setNewPlant(res.data);
+        setTimeout(() => navigate('/my-plants'), 4000);
       })
       .catch((err) => {
-        notify(false)
-        console.warn(err)
-      })
-  }
+        notify(false);
+        console.warn(err);
+      });
+  };
 
   const spinnerStructure = (
     <div id="spinner" className="flex flex-col items-center justify-center p-5">
       <div className="w-20 h-20 border-b-2 border-gray-900 rounded-full animate-spin"></div>
-      <p className='w-96 italic text-center p-5'>Plantasync data is hosted on a free cloud web-service. This may cause a response delay of up to 30 seconds for the first request.</p>
-    </div>)
+      <p className="w-96 italic text-center p-5">
+        Plantasync data is hosted on a free cloud web-service. This may cause a
+        response delay of up to 30 seconds for the first request.
+      </p>
+    </div>
+  );
 
   const getCommonNames = () => {
-    return explore.common ? explore.common[0] : ''
-  }
+    return explore.common ? explore.common[0] : '';
+  };
 
-    const notify = (result) => {
-    
-      return result ? toast.success(`We just added ${newplant.name} to your garden. Congrats! 🪴.`, {
-      position: "bottom-center",
-      autoClose: 3000,
-      hideProgressBar: false,
-      closeOnClick: false,
-      pauseOnHover: false,
-      draggable: false,
-      progress: undefined
-    }) : toast.error(`We were unable to add ${newplant.name} to your garden 🥲 Please check your internet and try again in a few minutes.`, {
-      position: "bottom-center",
-      autoClose: 3000,
-      hideProgressBar: false,
-      closeOnClick: false,
-      pauseOnHover: false,
-      draggable: false,
-      progress: undefined
-    })}
+  const notify = (result) => {
+    return result
+      ? toast.success(
+          `We just added ${newplant.name} to your garden. Congrats! 🪴.`,
+          {
+            position: 'bottom-center',
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: false,
+            draggable: false,
+            progress: undefined
+          }
+        )
+      : toast.error(
+          `We were unable to add ${newplant.name} to your garden 🥲 Please check your internet and try again in a few minutes.`,
+          {
+            position: 'bottom-center',
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: false,
+            draggable: false,
+            progress: undefined
+          }
+        );
+  };
 
   const showExploreStructure = (
     <section>
-      <div className='flex flex-col gap-1 place-items-center p-2 h-screen tablet:pt-8'>
-      <header className='relative flex items-center justify-center'>
-          <img className='grayscale-[50%] brightness-75 place-self-center static w-screen h-[275px] tablet:w-[400px] tablet:h-[400px]' src={`https://img.artpal.com/444151/15-20-2-28-3-14-30m.jpg`} alt='plant'>
-          </img>
+      <div className="flex flex-col tablet:flex-row tablet:gap-8 gap-1 place-items-center place-content-center p-2 h-screen max-h-[600px] tablet:py-12">
+        <header className="relative flex items-center justify-center tablet:place-self-start">
+          <img
+            className="grayscale-[50%] brightness-75 place-self-center static w-screen h-[275px] tablet:w-[400px] tablet:h-[400px]"
+            src={`https://img.artpal.com/444151/15-20-2-28-3-14-30m.jpg`}
+            alt="plant"
+          ></img>
           <h3 className="absolute text-white font-semibold text-[40px] text-center tablet:text-[40px]">
-              {getCommonNames()}
+            {getCommonNames()}
           </h3>
         </header>
-        <div className='flex flex-row gap-1 p-2 tablet:pb-8'>
-        <Link to={`/explore`}>
-            <button className='button-style mt-0 m-0 w-40 tablet:w-56 tablet:h-[50px]'>Back to List</button>
-        </Link>{' '}
-        <div onClick={handleSubmit} className='hover:cursor-pointer button-style m-0 w-40 tablet:w-56 flex flex-row gap-1 justify-center place-items-center shadow-xl p-1'>
-          <p>Add to Garden</p>
-          <img alt='plant' className='place-self-center w-[25px] h-[25px] tablet:w-[40px] tablet:h-[40px]' src='https://cdn-icons-png.flaticon.com/512/628/628324.png'/>
-        </div>
-        </div>
-        <div className='flex flex-col text-left p-2 pt-0 tablet:text-[18px]'>
-        <h1 className="font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-[#173d0a] to-[#64aa85] text-[32px] antialiased">
+        <div className="place-self-start tablet:pt-10">
+          <div className="flex flex-col text-left tablet:p-2 pt-0 tablet:text-[18px]">
+            <h1 className="font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-[#173d0a] to-[#64aa85] text-[32px] antialiased">
               Plant Details:
-            </h1>        
-        <p><strong>Known as: </strong><em>{explore.common ? `${explore.common.join(' & ')}` : ''}</em></p>
-        <p><strong>Latin name: </strong>{explore.latin}, from the {explore.family} Family</p>
-        <p ><strong>Ideal Light: </strong>{explore.ideallight}</p>
-        <p><strong>Water Tips: </strong>{explore.watering}</p> 
+            </h1>
+            <p className="pl-1">
+              <strong>Known as: </strong>
+              <em>{explore.common ? `${explore.common.join(' & ')}` : ''}</em>
+            </p>
+            <p className="pl-1">
+              <strong>Latin name: </strong>
+              {explore.latin}, from the {explore.family} Family
+            </p>
+            <p className="pl-1">
+              <strong>Ideal Light: </strong>
+              {explore.ideallight}
+            </p>
+            <p className="pl-1">
+              <strong>Water Tips: </strong>
+              {explore.watering}
+            </p>
+          </div>
+          <div className="flex w-[100%] place-items-center text-center flex-row gap-1 tablet:p-2 tablet:pb-8">
+            <Link
+              to={`/explore`}
+              className="flex place-content-center mt-0 m-0 w-[50%] button-style h-[40px] tablet:h-[50px] p-1"
+            >
+              <div className="place-self-center">Back to List</div>
+            </Link>{' '}
+            <div
+              onClick={handleSubmit}
+              className="hover:cursor-pointer w-[50%] h-[40px] tablet:h-[50px] button-style m-0 flex flex-row gap-1 justify-center place-items-center shadow-xl p-1"
+            >
+              <p>Add to Garden</p>
+              <img
+                alt="plant"
+                className="place-self-center w-[25px] h-[25px] tablet:w-[40px] tablet:h-[40px]"
+                src="https://cdn-icons-png.flaticon.com/512/628/628324.png"
+              />
+            </div>
+          </div>
         </div>
-        <div className='z-50'>
-        <ToastContainer
-          limit={1}
-          toastStyle={{ color: 'white', backgroundColor: 'black'}}
+        <div className="z-50">
+          <ToastContainer
+            limit={1}
+            toastStyle={{ color: 'white', backgroundColor: 'black' }}
           />
         </div>
       </div>
-        <Footer/>
     </section>
   );
   return explore.common ? showExploreStructure : spinnerStructure;
--- front-end/src/Pages/Footer.js
+++ front-end/src/Pages/Footer.js
@@ -1,34 +1,149 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Footer() {
+  const [toggleFooterOptions, setToggleFooterOptions] = useState({
+    navigation: false,
+    contact: false,
+    followUs: false
+  });
+  console.log(toggleFooterOptions);
+
   const handleClose = () => {
     document.body.scrollTop = document.documentElement.scrollTop = 0;
   };
 
-  return (
-    <div className="w-screen text-left flex flex-col place-content-between py-12 px-4 h-[300px] bg-[#9EC2AF]">
-      <div>
-        <Link to={'/'} onClick={handleClose}>
-          <h1 className="text-[rgb(23,61,10)] text-[32px] font-['brandon-grotesque'] px-[12%] font-bold antialiased tracking-wide uppercase">
-            Plantasynch
-          </h1>
+  const laptopViewLinks = (
+    <div className="hidden tablet:flex flex-row place-content-between text-slate-800">
+      <div className="flex flex-col tablet:gap-1">
+        <div className="text-[rgb(23,61,10)] pb-2 text-[20px] font-bold antialiased">
+          Navigation
+        </div>
+        <Link
+          to={'/my-plants'}
+          onClick={handleClose}
+          className="text-[15px] w-min delay-150 hover:underline hover:cursor-pointer"
+        >
+          Home
         </Link>
-        <p className="px-[12%]">Keep your plants alive</p>
-      </div>
+        <Link
+          to={'/explore'}
+          onClick={handleClose}
+          className="text-[15px] w-min delay-150 hover:underline hover:cursor-pointer"
+        >
+          Explore
+        </Link>
 
-      <div className="flex flex-col">
-        <div className="flex flex-row place-content-evenly text-slate-800 p-4 tablet:place-content-center uppercase">
+        <Link
+          to={'/new'}
+          onClick={handleClose}
+          className="text-[15px] w-min delay-150 hover:underline hover:cursor-pointer"
+        >
+          Add
+        </Link>
+
+        <Link
+          to={'/'}
+          onClick={handleClose}
+          className="text-[15px] w-min delay-150 hover:underline hover:cursor-pointer"
+        >
+          About
+        </Link>
+      </div>
+      <div className="flex flex-col tablet:gap-1">
+        <div className="text-[rgb(23,61,10)] pb-2 text-[20px] font-bold antialiased">
+          Contact
+        </div>
+        <Link
+          to={'/my-plants'}
+          onClick={handleClose}
+          className="text-[15px] delay-150 hover:underline hover:cursor-pointer"
+        >
+          Help Center
+        </Link>
+        <Link
+          to={'/explore'}
+          onClick={handleClose}
+          className="text-[15px] w-min delay-150 hover:underline hover:cursor-pointer"
+        >
+          Support
+        </Link>
+
+        <Link
+          to={'/new'}
+          onClick={handleClose}
+          className="text-[15px] delay-150 hover:underline hover:cursor-pointer"
+        >
+          Send E-mail
+        </Link>
+      </div>
+      <div className="flex flex-col tablet:gap-1">
+        <div className="text-[rgb(23,61,10)] pb-2 text-[20px] font-bold antialiased">
+          Follow us
+        </div>
+        <Link
+          to={'/my-plants'}
+          onClick={handleClose}
+          className="text-[15px] w-min delay-150 hover:underline hover:cursor-pointer"
+        >
+          Instagram
+        </Link>
+        <Link
+          to={'/explore'}
+          onClick={handleClose}
+          className="text-[15px] w-min delay-150 hover:underline hover:cursor-pointer"
+        >
+          Facebook
+        </Link>
+      </div>
+    </div>
+  );
+
+  const mobileViewLinks = (
+    <div className="flex tablet:hidden h-[180px] flex-col place-content-between text-slate-800">
+      <div className="flex flex-col tablet:gap-1">
+        <div
+          onClick={() => {
+            setToggleFooterOptions({
+              contact: false,
+              followUs: false,
+              navigation: !toggleFooterOptions.navigation
+            });
+          }}
+          className="text-[rgb(23,61,10)] flex flex-row place-content-between h-[40px] text-[20px] font-bold antialiased"
+        >
+          <div className="place-self-center">Navigation</div>
+          <svg
+            className={`w-[20px] h-[20px] place-self-center transition-all
+              ${toggleFooterOptions.navigation ? ' rotate-180' : ''} `}
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+          </svg>
+        </div>
+        <div
+          className={
+            toggleFooterOptions.navigation
+              ? 'flex flex-col absolute text-white w-[91.5%] rounded-lg bg-[#1E1F1D] mt-10'
+              : 'hidden'
+          }
+        >
           <Link
             to={'/my-plants'}
             onClick={handleClose}
-            className="text-xl delay-150 hover:text-slate-400 hover:cursor-pointer tablet:px-4"
+            className="text-[17px] px-4 p-2 delay-150 hover:underline hover:cursor-pointer"
           >
             Home
           </Link>
           <Link
             to={'/explore'}
             onClick={handleClose}
-            className="text-xl w-min delay-150 hover:text-slate-400 hover:cursor-pointer tablet:px-4"
+            className="text-[17px] px-4 p-2 delay-150 hover:underline hover:cursor-pointer"
           >
             Explore
           </Link>
@@ -36,7 +151,7 @@ export default function Footer() {
           <Link
             to={'/new'}
             onClick={handleClose}
-            className="text-xl w-min delay-150 hover:text-slate-400 hover:cursor-pointer tablet:px-4"
+            className="text-[17px] px-4 p-2 delay-150 hover:underline hover:cursor-pointer"
           >
             Add
           </Link>
@@ -44,40 +159,203 @@ export default function Footer() {
           <Link
             to={'/'}
             onClick={handleClose}
-            className="text-xl w-min delay-150 hover:text-slate-400 hover:cursor-pointer tablet:px-4"
+            className="text-[17px] px-4 p-2 delay-150 hover:underline hover:cursor-pointer"
           >
             About
           </Link>
         </div>
-        <div className="flex flex-row place-content-center gap-2">
-          <a
-            className="place-self-center"
-            href="https://github.com/JC-MT/Plantasynch"
-            target="_blank"
-            rel="noreferrer"
+      </div>
+      <div className="flex flex-col tablet:gap-1 overflow-hidden">
+        <div
+          onClick={() => {
+            setToggleFooterOptions({
+              navigation: false,
+              followUs: false,
+              contact: !toggleFooterOptions.contact
+            });
+          }}
+          className="text-[rgb(23,61,10)] flex flex-row place-content-between h-[40px] text-[20px] font-bold antialiased"
+        >
+          <div className="place-self-center">Contact us</div>
+          <svg
+            className={`w-[20px] h-[20px] place-self-center transition-all
+              ${toggleFooterOptions.contact ? ' rotate-180' : ''} `}
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
           >
-            <img
-              className="w-6 h-6 rounded-full place-self-center"
-              alt="GitHub"
-              src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
-            />
-          </a>
-          <a
-            className="place-self-center"
-            href="https://www.linkedin.com/in/jan-matias/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              className="w-8 h-8 place-self-center"
-              alt="LinkedIn"
-              src="https://www.pngmart.com/files/21/Linkedin-In-Logo-PNG-HD.png"
-            />
-          </a>
+            <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+          </svg>
         </div>
-        <p className="place-self-center text-sm tablet:p-4">
-          © 2023 Plantasynch, a project with no intention to profit.
-        </p>
+        <div
+          className={
+            toggleFooterOptions.contact
+              ? 'flex flex-col absolute text-white rounded-lg w-[91.5%] bg-[#1E1F1D] mt-10'
+              : 'hidden'
+          }
+        >
+          <Link
+            to={'/my-plants'}
+            onClick={handleClose}
+            className="text-[17px] px-4 p-2 delay-150 hover:underline hover:cursor-pointer"
+          >
+            Help Center
+          </Link>
+          <Link
+            to={'/explore'}
+            onClick={handleClose}
+            className="text-[17px] px-4 p-2 delay-150 hover:underline hover:cursor-pointer"
+          >
+            Support
+          </Link>
+
+          <Link
+            to={'/new'}
+            onClick={handleClose}
+            className="text-[17px] px-4 p-2 delay-150 hover:underline hover:cursor-pointer"
+          >
+            Send E-mail
+          </Link>
+        </div>
+      </div>
+      <div className="flex flex-col tablet:gap-1">
+        <div
+          onClick={() => {
+            setToggleFooterOptions({
+              navigation: false,
+              contact: false,
+              followUs: !toggleFooterOptions.followUs
+            });
+          }}
+          className="text-[rgb(23,61,10)] flex flex-row place-content-between h-[40px] text-[20px] font-bold antialiased"
+        >
+          <div className="place-self-center">Follow us</div>
+          <svg
+            className={`w-[20px] h-[20px] place-self-center transition-all
+              ${toggleFooterOptions.followUs ? ' rotate-180' : ''} `}
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+          </svg>
+        </div>
+        <div
+          className={
+            toggleFooterOptions.followUs
+              ? 'flex flex-col absolute text-white w-[91.5%] rounded-lg bg-[#1E1F1D] mt-10'
+              : 'hidden'
+          }
+        >
+          <Link
+            to={'/my-plants'}
+            onClick={handleClose}
+            className="text-[17px] px-4 p-2 delay-150 hover:underline hover:cursor-pointer"
+          >
+            Instagram
+          </Link>
+          <Link
+            to={'/explore'}
+            onClick={handleClose}
+            className="text-[17px] px-4 p-2 delay-150 hover:underline hover:cursor-pointer"
+          >
+            Facebook
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+
+  const icons = (
+    <div className="hidden tablet:flex flex-row place-content-start pl-1 gap-3">
+      <img
+        alt="logo"
+        className="cursor-pointer place-self-center w-[26px] h-[26px] hover:animate-[wiggle_2s_ease-in-out_infinite]"
+        src="https://cdn-icons-png.flaticon.com/512/628/628324.png"
+      />
+      <a
+        className="place-self-center"
+        href="https://github.com/JC-MT/Plantasynch"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          className="w-[22px] h-[22px] rounded-full place-self-center"
+          alt="GitHub"
+          src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+        />
+      </a>
+      <a
+        className="place-self-center"
+        href="https://www.linkedin.com/in/jan-matias/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          className="w-[27px] h-[27px] place-self-center"
+          alt="LinkedIn"
+          src="https://www.pngmart.com/files/21/Linkedin-In-Logo-PNG-HD.png"
+        />
+      </a>
+    </div>
+  );
+
+  return (
+    <div className="w-screen text-left flex flex-col tablet:flex-row tablet:place-content-between p-4 py-12 tablet:px-[10%] h-[560px] tablet:h-[400px] bg-[#9EC2AF]">
+      <div className="flex flex-col mb-10 tablet:mb-0 place-content-between tablet:w-1/2">
+        <Link to={'/'} onClick={handleClose}>
+          <h1 className="text-[rgb(23,61,10)] text-[32px] font-['brandon-grotesque'] font-bold antialiased tracking-wide uppercase">
+            Plantasynch
+          </h1>
+          <p className="pl-1">Keep your plants alive</p>
+        </Link>
+        {icons}
+      </div>
+      <div className="flex flex-col h-full place-content-between tablet:w-1/2">
+        {laptopViewLinks}
+        {mobileViewLinks}
+        <div className="flex flex-col place-content-between tablet:place-content-end h-[60px] tablet:h-full">
+          <div className="flex tablet:hidden flex-row place-content-center pl-1 gap-3">
+            <img
+              alt="logo"
+              className="cursor-pointer place-self-center w-[26px] h-[26px] hover:animate-[wiggle_2s_ease-in-out_infinite]"
+              src="https://cdn-icons-png.flaticon.com/512/628/628324.png"
+            />
+            <a
+              className="place-self-center"
+              href="https://github.com/JC-MT/Plantasynch"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="w-[22px] h-[22px] rounded-full place-self-center"
+                alt="GitHub"
+                src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+              />
+            </a>
+            <a
+              className="place-self-center"
+              href="https://www.linkedin.com/in/jan-matias/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="w-[27px] h-[27px] place-self-center"
+                alt="LinkedIn"
+                src="https://www.pngmart.com/files/21/Linkedin-In-Logo-PNG-HD.png"
+              />
+            </a>
+          </div>
+          <p className="text-center tablet:place-self-end tablet:text-right min-h-[27px] text-md">
+            Copyright © Plantasync.netlify.app 2023.
+          </p>
+        </div>
       </div>
     </div>
   );
