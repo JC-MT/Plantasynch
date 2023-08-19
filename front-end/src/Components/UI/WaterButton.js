27d0d2e3946f32f9f65d51b9639989fab692bf53
Jan Matias <jancarlosmatias@gmail.com>
2023-08-19 16:21:00 -0400

    Fixes WaterButton model position

--- front-end/src/Components/UI/WaterButton.js
+++ front-end/src/Components/UI/WaterButton.js
@@ -29,17 +29,19 @@ export default function WaterButton({ needsWater, last_water, plant }) {
     useWaterConfirmation({ handleUpdate, plant });
 
   return (
-    <div
-      onClick={() => handleWatering()}
-      className="hover:animate-[pulse_2s_ease-in-out_infinite] flex flex-row place-self-center place-items-center place-content-center button-style mt-0 w-44 h-12"
-    >
-      <span className="text-lg whitespace-nowrap">Water Plant</span>
-      <img
-        alt="water icon"
-        className="place-self-center hover:cursor-pointer w-[35px] h-[35px]"
-        src="https://cdn-icons-png.flaticon.com/512/2514/2514435.png"
-      ></img>
+    <>
+      <div
+        onClick={() => handleWatering()}
+        className="flex flex-row place-self-center place-items-center place-content-center button-style mt-0 w-44 h-12"
+      >
+        <span className="text-lg whitespace-nowrap">Water Plant</span>
+        <img
+          alt="water icon"
+          className="place-self-center hover:cursor-pointer w-[35px] h-[35px]"
+          src="https://cdn-icons-png.flaticon.com/512/2514/2514435.png"
+        ></img>
+      </div>
       {confirmation ? modelConfirmation : ''}
-    </div>
+    </>
   );
 }
--- front-end/src/Hooks/useWaterConfirmation.js
+++ front-end/src/Hooks/useWaterConfirmation.js
@@ -1,30 +1,30 @@
 import { useState } from 'react';
 
-export default function useWaterConfirmation({handleUpdate, plant}) {
+export default function useWaterConfirmation({ handleUpdate, plant }) {
   const [confirmation, setConfirmation] = useState(false);
 
   const modelConfirmation = (
-      <div className="bg-slate-800 bg-opacity-50 flex justify-center items-center fixed left-0 bottom-0 w-screen h-screen z-40 pt-0">
-        <div className="bg-white px-8 py-10 rounded-md text-center w-[90%] max-w-[600px] tracking-wide font-normal">
-          <h1 className="text-xl mb-4 font-bold text-slate-500">
-            Looks like {plant.name} is not ready to be watered. Are you sure you want to water this plant?
-          </h1>
-          <button
-            onClick={() => setConfirmation(false)}
-            className="bg-red-500 px-2 py-2 rounded-md text-md tracking-wider text-white hover:bg-red-600"
-          >
-            Don't water
-          </button>
-          <button
-            onClick={() => handleUpdate()}
-            className="bg-green-500 hover:bg-green-600 px-2 py-2 ml-2 tracking-wider rounded-md text-md text-white"
-          >
-            Yes, please
-          </button>
-        </div>
-        
+    <div className="bg-slate-800 bg-opacity-50 flex justify-center items-center fixed left-0 bottom-0 w-screen h-screen z-50 pt-0">
+      <div className="bg-white px-8 py-10 rounded-md text-center w-[90%] max-w-[600px] tracking-wide font-normal">
+        <h1 className="text-xl mb-4 font-bold text-slate-500">
+          Looks like {plant.name} is not ready to be watered. Are you sure you
+          want to water this plant?
+        </h1>
+        <button
+          onClick={() => setConfirmation(false)}
+          className="bg-red-500 px-2 py-2 rounded-md text-md tracking-wider text-white hover:bg-red-600"
+        >
+          Don't water
+        </button>
+        <button
+          onClick={() => handleUpdate()}
+          className="bg-green-500 hover:bg-green-600 px-2 py-2 ml-2 tracking-wider rounded-md text-md text-white"
+        >
+          Yes, please
+        </button>
       </div>
-    );
+    </div>
+  );
 
   return [confirmation, setConfirmation, modelConfirmation];
 }
